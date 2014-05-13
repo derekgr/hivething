e661fd23b80a60e6cb5b921eda6b22fed3be0b40
Derek Greentree <derek@github.com>
2014-05-13 08:29:20 -0600

    sql driver spike

--- main.go
+++ main.go
@@ -12,27 +12,36 @@ import (
 	"git.apache.org/thrift.git/lib/go/thrift"
 )
 
-type Conn struct {
-	Hive          *tcliservice.TCLIServiceClient
-	SessionHandle *tcliservice.TSessionHandle
+type conn struct {
+	hive          *tcliservice.TCLIServiceClient
+	sessionHandle *tcliservice.TSessionHandle
+	options       *ConnOptions
 }
 
-type Stmt struct {
-	Statement  *tcliservice.TExecuteStatementReq
-	Connection *Conn
+type ConnOptions struct {
+	QueryPagination int
 }
 
-type Rows struct {
-	Handle *tcliservice.TOperationHandle
+type stmt struct {
+	hive      *tcliservice.TCLIServiceClient
+	statement *tcliservice.TExecuteStatementReq
 }
 
-type Driver struct{}
+type rows struct {
+	hive   *tcliservice.TCLIServiceClient
+	handle *tcliservice.TOperationHandle
+	rowSet *tcliservice.TRowSet
+}
 
-/*
-Implementation method for driver.Driver.
-*/
+type hivedriver struct {
+	options *ConnOptions
+}
 
-func (d *Driver) Open(host string) (driver.Conn, error) {
+func NewDriver(options *ConnOptions) driver.Driver {
+	return &hivedriver{options}
+}
+
+func (d *hivedriver) Open(host string) (driver.Conn, error) {
 	transport, err := thrift.NewTSocket(host)
 	if err != nil {
 		return nil, err
@@ -56,44 +65,44 @@ func (d *Driver) Open(host string) (driver.Conn, error) {
 
 	session, err := client.OpenSession(*tcliservice.NewTOpenSessionReq())
 	if err != nil {
-		return nil, fmt.Errorf("Error opening session: ", err)
+		return nil, err
 	}
 
-	return &Conn{client, session.SessionHandle}, nil
+	return &conn{client, session.SessionHandle, d.options}, nil
 }
 
-func (c *Conn) isOpen() bool {
-	return c.SessionHandle != nil
+func (c *conn) isOpen() bool {
+	return c.sessionHandle != nil
 }
 
 /*
 Implementation methods for driver.Conn.
 */
 
-func (c *Conn) Prepare(query string) (driver.Stmt, error) {
+func (c *conn) Prepare(query string) (driver.Stmt, error) {
 	executeReq := tcliservice.NewTExecuteStatementReq()
-	executeReq.SessionHandle = *c.SessionHandle
+	executeReq.SessionHandle = *c.sessionHandle
 	executeReq.Statement = query
 
-	return &Stmt{executeReq, c}, nil
+	return &stmt{c.hive, executeReq}, nil
 }
 
-func (c *Conn) Close() error {
+func (c *conn) Close() error {
 	if c.isOpen() {
 		closeReq := tcliservice.NewTCloseSessionReq()
-		closeReq.SessionHandle = *c.SessionHandle
-		resp, err := c.Hive.CloseSession(*closeReq)
+		closeReq.SessionHandle = *c.sessionHandle
+		resp, err := c.hive.CloseSession(*closeReq)
 		if err != nil {
 			return fmt.Errorf("Error closing session: ", resp, err)
 		}
 
-		c.SessionHandle = nil
+		c.sessionHandle = nil
 	}
 
 	return nil
 }
 
-func (c *Conn) Begin() (driver.Tx, error) {
+func (c *conn) Begin() (driver.Tx, error) {
 	// Hmm. Hive isn't transactional...
 	return nil, nil
 }
@@ -102,24 +111,24 @@ func (c *Conn) Begin() (driver.Tx, error) {
 Implementation methods for driver.Stmt.
 */
 
-func (s *Stmt) Close() error {
+func (s *stmt) Close() error {
 	// No such thing.
 	return nil
 }
 
-func (s *Stmt) NumInput() int {
+func (s *stmt) NumInput() int {
 	// TODO: Parse the query for :placeholder_parameters, count them,
 	// return the total here.
 	return -1
 }
 
-func (s *Stmt) Exec(args []driver.Value) (driver.Result, error) {
+func (s *stmt) Exec(args []driver.Value) (driver.Result, error) {
 	// Let's just read-only for now.
 	return nil, fmt.Errorf("INSERTs and UPDATEs are not supported!")
 }
 
-func (s *Stmt) Query(args []driver.Value) (driver.Rows, error) {
-	response, err := s.Connection.Hive.ExecuteStatement(*s.Statement)
+func (s *stmt) Query(args []driver.Value) (driver.Rows, error) {
+	response, err := s.hive.ExecuteStatement(*s.statement)
 	if err != nil {
 		return nil, fmt.Errorf("Error in ExecuteStatement: ", response, err)
 	}
@@ -129,27 +138,35 @@ func (s *Stmt) Query(args []driver.Value) (driver.Rows, error) {
 		return nil, fmt.Errorf("Error from server: ", response.Status)
 	}
 
-	return &Rows{response.OperationHandle}, nil
+	return &rows{s.hive, response.OperationHandle, nil}, nil
 }
 
 /*
 Implementation for driver.Rows.
 */
 
-func (r *Rows) Columns() []string {
+func (r *rows) Columns() []string {
+	if r.rowSet == nil {
+		r.Next(nil)
+	}
+
+	cols := make([]string, len(r.rowSet.Columns))
+	for _, c := range r.rowSet.Columns {
+		append(cols, c.String())
+	}
+	return cols
+}
+
+func (r *rows) Close() error {
 	return nil
 }
 
-func (r *Rows) Close() error {
-	return nil
-}
-
-func (r *Rows) Next(dest []driver.Value) error {
+func (r *rows) Next(dest []driver.Value) error {
 	return io.EOF
 }
 
 func main() {
-	sql.Register("hive", &Driver{})
+	sql.Register("hive", NewDriver(&ConnOptions{QueryPagination: 1000}))
 
 	db, err := sql.Open("hive", "127.0.0.1:10000")
 	if err != nil {
